9f5e91a26f16c92d95194667d7fecced7c0d6f8c
platothegeek <platothegeek@gmail.com>
2020-12-02 15:15:50 -0800

    Added progress bars (problem with age bar starting populated

--- java/app.js
+++ java/app.js
@@ -1,4 +1,4 @@
-rand = (Math.floor((Math.random() * 4000)));
+rand = (Math.floor((Math.random() * 400)));
 let myTama = {
     name: 0,
     age: 0,
@@ -17,50 +17,51 @@ let myTama = {
         myTama.name = $("#tamaNameInput").val();
         $("#tamaName").html("<h2 id='tamaName'>" + myTama.name + "&nbsp;</h2>");
         myTama.ager = setInterval(ageUp,2000);
-        myTama.noFood = setInterval(addHung, 2500);
-        myTama.noSleep = setInterval(addTired, 7500);
-        myTama.noFun = setInterval(addBored, (rand + 1000));
+        myTama.noFood = setInterval(addHung, 250);
+        myTama.noSleep = setInterval(addTired, 750);
+        myTama.noFun = setInterval(addBored, (rand + 100));
     }
 }
 $("#startButton").click(myTama.gameBegin);
-ageUp = function ageUP() {
+ageUp = function ageUp() {
     $("#tamaAge").html("<h2 id='tamaAge'>" + (myTama.age+1) + "&nbsp;</h2>");
+    $("#ageBar>div").width(myTama.age + "%");
     myTama.age = myTama.age + 1;
     checkVitals();
 }
 addHung = function addHung() {
     $("#tamaHungies").html("<h2 id='tamaHungies'>" + (myTama.hunger+1) + "&nbsp;</h2>");
-    $("#tamaHungiesBar").val((myTama.hunger * 10)+10);
+    $("#hungerBar>div").width(myTama.hunger + "%");
     myTama.hunger = myTama.hunger + 1;
     checkVitals();
 }
 addTired = function addTired() {
     $("#tamaSleepies").html("<h2 id='tamaSleepies'>" + (myTama.sleep+1) + "&nbsp;</h2>")
-    $("#tamaSleepiesBar").val((myTama.sleep *10) + 10);
+    $("#sleepBar>div").width(myTama.sleep + "%");
     myTama.sleep = myTama.sleep + 1;
     checkVitals();
 }
 addBored = function addBored() {
     $("#tamaBoredom").html("<h2 id='tamaBoredom'>" + (myTama.bored+1) + "&nbsp;</h2>")
-    $("#tamaBoredomBar").val((myTama.bored *10) + 10);
+    $("#boredBar>div").width(myTama.bored + "%");
     myTama.bored = myTama.bored + 1;
     checkVitals();
 }
 checkVitals = function checkVitals() {
-    if (myTama.age==30){
+    if (myTama.age==50){
         $("#deadNotification").html(myTama.name + " is getting older.")
         $(".display").html("<img src='https://media4.giphy.com/media/7Cr71vflxfGFO/source.gif' alt='Kitten Gif' id='displayCat'>")
     }
-    else if(myTama.bored==7){
+    else if(myTama.bored==85){
         $("#deadNotification").html(myTama.name + "'s boredom is dangerously high.")
     }
-    else if(myTama.hunger==7){
+    else if(myTama.hunger==85){
         $("#deadNotification").html(myTama.name + " is dangerously close to starving.")
     }
-    else if(myTama.sleep==7){
+    else if(myTama.sleep==85){
         $("#deadNotification").html(myTama.name + " is very tired, let him sleep.")
     }
-    else if (myTama.age>=60) {
+    else if (myTama.age>=100) {
         clearInterval(myTama.ager);
         clearInterval(myTama.noFood);
         clearInterval(myTama.noSleep);
@@ -69,7 +70,7 @@ checkVitals = function checkVitals() {
         $("#deadNotification").html(myTama.name + " died of old age.<br>You 'win'.")
         $(".display").html("<img src='https://preview.pixlr.com/images/800wm/100/1/1001519923.jpg' alt='Kitten Gif' id='displayDead'>")
     }
-    else if (myTama.hunger>=10) {
+    else if (myTama.hunger>=100) {
         clearInterval(myTama.ager);
         clearInterval(myTama.noFood);
         clearInterval(myTama.noSleep);
@@ -78,7 +79,7 @@ checkVitals = function checkVitals() {
         $("#deadNotification").html(myTama.name + " died of starvation. <br>You've been arrested for animal cruelty.")
         $(".display").html("<img src='https://preview.pixlr.com/images/800wm/100/1/1001519923.jpg' alt='Kitten Gif' id='displayDead'>")
     }
-    else if (myTama.sleep>=10) {
+    else if (myTama.sleep>=100) {
         clearInterval(myTama.ager);
         clearInterval(myTama.noFood);
         clearInterval(myTama.noSleep);
@@ -87,29 +88,29 @@ checkVitals = function checkVitals() {
         $("#deadNotification").html(myTama.name + " died of exhaustion.<br> You should've let him sleep.")
         $(".display").html("<img src='https://preview.pixlr.com/images/800wm/100/1/1001519923.jpg' alt='Kitten Gif' id='displayDead'>")
     }
-    else if (myTama.bored>=10) {
+    else if (myTama.bored>=100) {
         clearInterval(myTama.ager);
         clearInterval(myTama.noFood);
         clearInterval(myTama.noSleep);
         clearInterval(myTama.noFun);
         console.log("he like dead tho");
-        $("#deadNotification").html(myTama.name + " literally died of boredom. <br> You  made " + myTama.name + "'s life so miserable he died.")
+        $("#deadNotification").html(myTama.name + " literally died of boredom. <br> You  should've sent him to General Assembly.")
         $(".display").html("<img src='https://preview.pixlr.com/images/800wm/100/1/1001519923.jpg' alt='Kitten Gif' id='displayDead'>")
     }
 }
 clearHung = function clearHung() {
     myTama.hunger = 0;
-    $("#tamaHungiesBar").val(1);
+    $("#hungerBar").width(0);
     $("#tamaHungies").html("<h2 id='tamaHungies'>" + (myTama.hunger) + "&nbsp;</h2>")
 }
 clearTired = function clearTired() {
     myTama.sleep = 0;
-    $("#tamaSleepiesBar").val(1);
+    $("#sleepBar").width(0);
     $("#tamaSleepies").html("<h2 id='tamaSleepies'>" + (myTama.sleep) + "&nbsp;</h2>")
 }
 clearBored = function clearBored() {
     myTama.bored = 0;
-    $("#tamaBoredomBar").val(1);
+    $("#boredBar").width(0);
     $("#tamaBoredom").html("<h2 id='tamaBoredom'>" + (myTama.bored) + "&nbsp;</h2>")
 }
 $("#foodButton").click(clearHung);
